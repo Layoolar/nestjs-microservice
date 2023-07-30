33dd5832107e8206b5fe737ceed70c42a3defde6
Layoolar <olaiwolaayoola@yahoo.com>
2023-07-30 21:50:43 +0100

    Set up requirements for user

--- apps/users/src/users.module.ts
+++ apps/users/src/users.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import * as Joi from 'joi'
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    validationSchema: Joi.object({
+      MONGODB_URI: Joi.string().required(),
+    }),
+    envFilePath: './apps/users/.env',
+  }),
+  DatabaseModule,
+],
   controllers: [UsersController],
   providers: [UsersService],
 })
